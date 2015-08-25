c87e6153528caee3a55a363c4bafcbe3fccaf1d4
Your Name <you@example.com>
2015-08-25 21:37:11 +0600

    init

---
+++ trunk/protected/views/layouts/main.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Недвижимость в Мсокве</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">    
+    <script src="http://code.jquery.com/jquery-1.7.1.min.js" type="text/javascript"></script>
+    <script src="js/ff_cash.js" type="text/javascript"></script>
+    <script src="js/superfish.js" type="text/javascript"></script>
+	<script src="js/jquery.easing.1.3.js" type="text/javascript"></script>
+    <script src="js/tms-0.3.1.js" type="text/javascript"></script>
+    <script src="js/tms_presets.js" type="text/javascript"></script>
+    <script src="js/jquery.prettyPhoto.js" type="text/javascript"></script>
+    <script src="js/jquery.equalheights.js" type="text/javascript"></script>
+	<!--[if lt IE 7]> 
+  <div style='clear:both; text-align:center; position:relative;'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img                       src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a  faster, safer browsing experience, upgrade for free today." /></a></div>
+ <![endif]-->
+    <!--[if lt IE 9]>
+   		<script type="text/javascript" src="js/html5.js"></script>
+        <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+	<![endif]-->
+</head>
+<body>
+<!--==============================header=================================-->
+<header class="header">
+	<div class="row-1">
+    	<div class="main">
+        	<h1><a href="#"><strong>MSK</strong><em>недвижимость</em></a><span>у вас есть на кого полжиться в Москве</span></h1>
+            <ul class="menu-top">
+            	<li><a href="#">Заказать звонок</a></li>
+                <li><a href="#">Помощь</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="row-2">
+    	<div class="main">
+        	<nav>
+                <ul class="sf-menu">
+                    <li><a class="current" href="#"><strong><strong>Главная</strong></strong></a></li>
+                    <li><a href="#"><strong><strong>Услуги</strong></strong></a>
+                    	<ul>
+                            <li><a href="#">Покупка недвижимости</a></li>
+                            <li><a href="#">Продажа недвижимости</a></li>
+                            <li><a href="#">Расселение</a></li>
+                            <li><a href="#">Конструктор услуг</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#"><strong><strong>Контакты</strong></strong></a></li>
+              </ul>
+          </nav>
+        </div>
+    </div>
+    <div class="row-3">
+    	<div class="main">
+        	<div class="bg-slider">
+                <div class="slider">
+                    <ul class="items">
+                        <li><img src="images/slide-1.jpg" alt="">
+                            <div class="banner banner1">
+                                <strong class="text1-banner"><strong>П</strong><span>оиск</span></strong>
+                                <strong class="text2-banner">и подбор недвижимости</strong>
+                                <p class="indent-bot">наши специалисты проведут весь комплекс услуг по поиску и подбору недвижимости иключительно под Ваши требования</p>
+                                <span class="button1-shadow"><a href="#" class="button-1">Подробнее</a></span>
+                            </div>
+                        </li>
+                        <li><img src="images/slide-2.jpg" alt="">
+                            <div class="banner banner2">
+                                <strong class="text1-banner">Юридическая</strong>
+                                <strong class="text2-banner">проверка</strong>
+								<p class="indent-bot">
+									Описание и текст.. душетрепещущие лозунги и
+									красочные обещания, увидев которые люди
+									перестают понимать как они жили без
+									этого до этого..
+								</p>
+                                <span class="button1-shadow"><a href="#" class="button-1">Подробнее</a></span>
+                            </div>
+                        </li>
+                        <li><img src="images/slide-3.jpg" alt="">
+                            <div class="banner banner3">
+                                <strong class="text1-banner">Проведение</strong>
+                                <strong class="text2-banner">Сделки</strong>
+								<p class="indent-bot">
+									Описание и текст.. душетрепещущие лозунги и
+									красочные обещания, увидев которые люди
+									перестают понимать как они жили без
+									этого до этого..
+								</p>
+                                <span class="button1-shadow"><a href="#" class="button-1">Подробнее</a></span>
+                            </div>
+                        </li>
+                        <li><img src="images/slide-4.jpg" alt="">
+                            <div class="banner banner4">
+                                <strong class="text1-banner"><strong>П</strong><span>омощь</span></strong>
+                                <strong class="text2-banner">в заселении</strong>
+                                <p class="indent-bot">
+									Описание и текст.. душетрепещущие лозунги и
+									красочные обещания, увидев которые люди
+									перестают понимать как они жили без
+									этого до этого..
+								</p>
+                                <span class="button1-shadow"><a href="#" class="button-1">Подробнее</a></span>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul class="pagination">
+                        <li><a class="pags1" href="#"><strong>Поиск</strong> и подбор</a><span></span></li>
+                        <li><a class="pags2" href="#"><strong>Проверка</strong></a><span></span></li>
+                        <li><a class="pags3" href="#"><strong>Сделка</strong></a><span></span></li>
+                        <li><a class="pags4" href="#">Заселение</a><span></span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!--==============================End header=================================-->
+
+<!--==============================content================================-->
+<section id="content">
+	<div class="padding-content">
+    	<div class="container_24">
+        	<div class="wrapper p4">
+            	<article class="grid_8">
+                	<div class="box maxheight">
+                        <div class="padding-box">
+                        	<h3 class="spacing-bot">ПОИСК ОБЪЕКТА ПО ПАРАМЕТРАМ</h3>
+                            <figure class="spacing-bot"><a class="lightbox-image" href="images/image-blank.png" data-gal="prettyPhoto[gallery1]"><img src="images/page1-img1.jpg" width="250" height="132" alt=""></a></figure>
+                            <p class="p2">
+								Комфорт в порядке вещей.<br/>
+								Мы, понимая потребности наших клиентов,
+								предлагаем воспользоваться услугами
+								следующих профессионалов своего дела:
+							</p>
+                            <ul class="list-1 spacing-bot">
+                            	<li><a href="#">Вывоз мусора</a></li>
+								<li><a href="#">Перевоз мебели</a></li>
+								<li><a href="#">Сборка мебели</a></li>
+								<li><a href="#">Ремонт</a></li>
+								<li><a href="#">Установка окон</a></li>
+								<li><a href="#">Установка сигнализаций</a></li>
+								<li><a href="#">Установка сантехнического оборудования</a></li>
+								<li class="last-item"><a href="#">Адвокат, юрист</a></li>
+                            </ul>
+                            <a href="#" class="button">Подробнее</a>
+                        </div>
+                    </div>
+                </article>
+                <article class="grid_8">
+                	<div class="box maxheight">
+                        <div class="padding-box">
+                        	<h3 class="spacing-bot">Расселение</h3>
+                            <figure class="spacing-bot"><a class="lightbox-image" href="images/image-blank.png" data-gal="prettyPhoto[gallery1]"><img src="images/page1-img2.jpg" width="250" height="132" alt=""></a></figure>
+                            <p class="p3">
+								Описание и текст.. душетрепещущие лозунги и
+								красочные обещания, увидев которые люди
+								перестают понимать как они жили без
+								этого до этого..
+							</p>
+							<ul class="list-1 spacing-bot">
+                            	<li><a href="#">Анализ рынка</a></li>
+								<li><a href="#">Поиск фльтернативных вариантов</a></li>
+								<li><a href="#">Подбор по параметрам</a></li>
+								<li><a href="#">Проверка документов</a></li>
+								<li><a href="#">Организация сделки</a></li>
+								<li class="last-item"><a href="#">Помощь в переезде</a></li>
+                            </ul>
+                            <a href="#" class="button">Подробнее</a>
+                        </div>
+                    </div>
+                </article>
+                <article class="grid_8">
+                	<div class="box maxheight">
+                        <div class="padding-box">
+                        	<h3 class="spacing-bot">Консультации</h3>
+                            <figure class="spacing-bot"><a class="lightbox-image" href="images/image-blank.png" data-gal="prettyPhoto[gallery1]"><img src="images/page1-img3.jpg" width="250" height="132" alt=""></a></figure>
+                            <p class="p3">
+								Описание и текст.. душетрепещущие лозунги и
+								красочные обещания, увидев которые люди
+								перестают понимать как они жили без
+								этого до этого..
+							</p>
+							<ul class="list-1 spacing-bot">
+                            	<li><a href="#">Часто задаваемые вопросы</a></li>
+								<li><a href="#">Что нужно знать при покупке</a></li>
+								<li><a href="#">Как не попасть в лапы мошенников</a></li>
+								<li><a href="#">Подводные камни на сделке</a></li>
+                            </ul>
+
+							<a href="#" class="button">Заказать звонок</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            <div class="wrapper">
+            	<article class="grid_16">
+                	<div class="box">
+                    	<div class="padding1-box">
+                        	<h3 class="indent-bot">Основные услуги</h3>
+                            <div class="wrapper spacer-1">
+                            	<div class="page1 col-1">
+                                	<h6>Покупка/Продажа</h6>
+                                    <p class="spacing-bot">
+										Описание и текст.. душетрепещущие лозунги и
+										красочные обещания, увидев которые люди
+										перестают понимать как они жили без
+										этого до этого..
+									</p>
+                                    <a href="#" class="button">Подробнее</a>
+                                </div>
+                                <div class="page1 col-2">
+                                	<h6>Консалтинг и сопровождение</h6>
+									<p class="spacing-bot">
+										Описание и текст.. душетрепещущие лозунги и
+										красочные обещания, увидев которые люди
+										перестают понимать как они жили без
+										этого до этого..
+									</p>
+
+                                    <a href="#" class="button">Подробнее</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article class="grid_8">
+                	<div class="box-1">
+                    	<div class="padding-box1">
+                        	<h3 class="spacing-bot">Конструктор</h3>
+                            <div class="box-quote">
+                            	<div class="padding">
+									Описание и текст.. душетрепещущие лозунги и
+									красочные обещания, увидев которые люди
+									перестают понимать как они жили без
+									этого до этого..
+                                </div>
+                                <span></span>
+                            </div>
+                            <div class="author">
+                            	Ничего лишнего <a href="#" class="link-1">Подобрать</a>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
+<!--==============================End content================================-->
+
+<!--==============================aside================================-->
+<aside>
+	<div class="container_24">
+    	<div class="wrapper">
+        	<article class="grid_8">
+            	<h3 class="p1">Покупка</h3>
+            	<ul class="list-2">
+                	<li><a href="#">Анализ рынка</a></li>
+                    <li><a href="#">Поиск эксклюзивных вариантов</a></li>
+                    <li><a href="#">Подбор по параметрам</a></li>
+                    <li><a href="#">Проверка документов</a></li>
+                    <li><a href="#">Организация сделки</a></li>
+                    <li class="last-item"><a href="#">Помощь в переезде</a></li>
+                </ul>
+            </article>
+            <article class="grid_8">
+            	<h3 class="p1">Продажа</h3>
+            	<ul class="list-2">
+                	<li><a href="#">Анализ рынка</a></li>
+                    <li><a href="#">Поиск эксклюзивных вариантов</a></li>
+                    <li><a href="#">Подбор по параметрам</a></li>
+                    <li><a href="#">Проверка документов</a></li>
+                    <li><a href="#">Организация сделки</a></li>
+                    <li class="last-item"><a href="#">Помощь в переезде</a></li>
+                </ul>
+            </article>
+            <article class="grid_8">
+            	<h3 class="p1">Сопровождение</h3>
+            	<ul class="list-2">
+                	<li><a href="#">Анализ рынка</a></li>
+                    <li><a href="#">Поиск эксклюзивных вариантов</a></li>
+                    <li><a href="#">Подбор по параметрам</a></li>
+                    <li><a href="#">Проверка документов</a></li>
+                    <li><a href="#">Организация сделки</a></li>
+                    <li class="last-item"><a href="#">Помощь в переезде</a></li>
+                </ul>
+            </article>
+        </div>
+    </div>
+</aside>
+<!--==============================End aside================================-->
+
+<!--==============================footer=================================-->
+<footer>
+	<div class="main">
+    	<div class="wrapper">
+        	<div class="text-bot">
+            	MSKнедвижимость &copy; 2011 <a href="#">квартиры в москве и подмосковье</a>
+            </div>
+            <div class="footer-link">
+            	<!-- {%FOOTER_LINK} -->
+            </div>
+        </div>
+    </div>
+</footer>
+<!--==============================End footer=================================-->
+</body>
+</html>
